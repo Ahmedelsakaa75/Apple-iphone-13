d169f76503ad66373a3aa6adfd0ea10091611b3d
Ahmed Elsakaa <95256404+Ahmedelsakaa75@users.noreply.github.com>
2022-08-22 17:27:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let mainImg = document.querySelector(".mainimg");
+let container = document.querySelector(".container")
+function phones(phone) {
+  mainImg.src = phone;
+}
+function colors(color){
+    container.style.background = color
+}
